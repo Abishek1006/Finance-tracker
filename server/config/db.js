916fa0c0eb514d2066d018092d2ca60628ea0358
Abishek1006 <abishek.nights@gmail.com>
2024-10-01 10:05:46 +0530

    First commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://abishek2201006:a0QtLBxI3JEmcc9A@financetracker.dabhg.mongodb.net/finance_tracker?retryWrites=true&w=majority', {
+    await mongoose.connect('your MongoDB URL here', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
